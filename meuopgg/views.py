98854b5af1942edfd67244525ba2e7e6b7516158
henriquecedraz2002@gmail.com <henriquecedraz2002@gmail.com>
2019-08-29 15:03:08 -0300

    mudança api key

--- meuopgg/views.py
+++ meuopgg/views.py
@@ -12,7 +12,7 @@ def level(request):
         invocador = summoner()
         summonerName = invocador.summonerName = request.POST.get('summonerName')
         region = request.POST.get('region')
-        takeApi = requests.get('https://' + region + '.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + summonerName + '?api_key=RGAPI-4eac7039-7560-4fae-9c4e-a58facd6e2a1')
+        takeApi = requests.get('https://' + region + '.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + summonerName + '?api_key=RGAPI-1101085a-49ff-457c-a039-884e2e63935f')
         apiDict = takeApi.json()
         summonerLevel = apiDict['summonerLevel']
         return render(request,'level.html', {'summonerName': summonerName, 'summonerLevel': summonerLevel})
