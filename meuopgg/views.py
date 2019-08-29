f5f247c602237ae5fef8e721e9b6dd2a6cfea466
henriquecedraz2002@gmail.com <henriquecedraz2002@gmail.com>
2019-08-29 16:07:56 -0300

    add rank and division

--- meuopgg/views.py
+++ meuopgg/views.py
@@ -15,4 +15,11 @@ def level(request):
         takeApi = requests.get('https://' + region + '.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + summonerName + '?api_key=RGAPI-1101085a-49ff-457c-a039-884e2e63935f')
         apiDict = takeApi.json()
         summonerLevel = apiDict['summonerLevel']
-        return render(request,'level.html', {'summonerName': summonerName, 'summonerLevel': summonerLevel})
+        summonerId = apiDict['id']
+        accountId = apiDict['accountId']
+        takeApiRank = requests.get('https://' + region + '.api.riotgames.com/lol/league/v4/entries/by-summoner/' + summonerId + '/?api_key=RGAPI-1101085a-49ff-457c-a039-884e2e63935f')
+        takeApiRankList = takeApiRank.json()
+        takeApiRankDict = takeApiRankList[1]
+        tier = takeApiRankDict['tier']
+        rank = takeApiRankDict['rank']
+        return render(request,'level.html', {'summonerName': summonerName, 'summonerLevel': summonerLevel, 'summonerId': summonerId, 'accountId': accountId, 'tier': tier, 'rank': rank})
